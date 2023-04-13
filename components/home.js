e38329e90656d2478bcf9ad2bd5a2c913c0f9566
Srahul-0918 <129747952+Srahul-0918@users.noreply.github.com>
2023-04-13 16:04:20 +0530

    Create home.js

---
+++ components/home.js
@@ -0,0 +1,15 @@
+import { Helmet } from 'react-helmet';
+const Home=()=>{
+    return (
+        <div><div>
+            <Helmet>
+      <title>Welcome    </title>
+      <meta name="description" content="yeh home hey"/>
+      <meta name="keywords" content="login,Srahul,Environment,SaveEarth,Save world"></meta>
+    </Helmet>
+        </div>
+          <h1> Welcome To Home page</h1> 
+        </div>
+    );
+}
+export default Home;
